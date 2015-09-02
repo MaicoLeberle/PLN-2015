ad29ebef279a7cb366af6d81691f0c25cd3c74ee
maico <maico@maico>
2015-09-02 12:32:05 -0300

    Ejercicio 1: Corpus

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -12,7 +12,7 @@ Options:
 from docopt import docopt
 import pickle
 
-from nltk.corpus import gutenberg
+from nltk.corpus import brown
 
 from languagemodeling.ngram import NGram
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = gutenberg.sents('austen-emma.txt')
+    sents = brown.sents()
 
     # train the model
     n = int(opts['-n'])
