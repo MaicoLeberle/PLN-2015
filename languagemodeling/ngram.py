223570c53572a796c83ca68ea929eeb2453b48d0
maico <maico@maico>
2015-09-10 21:46:07 -0300

    AddOneNGram class.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -3,14 +3,6 @@ from collections import defaultdict
 from math import log
 from random import uniform
 
-def first_non_extra(ngram):
-    res = ""
-    for i in range(len(ngram)):
-        if ngram[i] != "<s>":
-            res = ngram[i]
-            break
-    return res  
-
 class NGram(object):
 
     def __init__(self, n, sents):
@@ -25,14 +17,14 @@ class NGram(object):
         self.total_count = 0
         self.probs = defaultdict(int)
         self.sorted_probs = defaultdict(int)
-        lengths = defaultdict(int)
+        self.number_token_options = defaultdict(int)
 
         for i in range(len(sents)):
             sents[i] = (['<s>'] * (n - 1)) + sents[i] + (['</s>'])
 
         for sent in sents:
             for i in range(len(sent) - n + 1):
-                self.word_counts[first_non_extra(sent[i:i+n])] += 1
+                self.word_counts[sent[i+n-1]] += 1
                 self.total_count += 1
                 ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
@@ -41,13 +33,13 @@ class NGram(object):
                 if not ngram[:-1] in self.probs:
                     self.probs[ngram[:-1]] = defaultdict(int)
                 (self.probs[ngram[:-1]])[ngram[len(ngram) - 1]] += 1
-                lengths[ngram[:-1]] += 1
+                self.number_token_options[ngram[:-1]] += 1
 
         for key, value in self.probs.items():
             # Turn the number of occurrences into probabilities (that sum to 1,
             # all together) inside each n-gram.
             for k, v in value.items():
-                value[k] = v / float(lengths[key])
+                value[k] = v / float(self.number_token_options[key])
 
         for key, value in self.probs.items():
             # Once self.probs is fully processed, self.sorted_probs is easily
@@ -71,11 +63,9 @@ class NGram(object):
         token -- the token.
         prev_tokens -- the previous n-1 tokens (optional only if n = 1).
         """
-
         if prev_tokens is None:
             prev_tokens = []
 
-        
         ngram = tuple((['<s>'] * (self.n - 1 - len(prev_tokens))) + prev_tokens + [token])
         
         if float(self.counts[tuple(prev_tokens)]) == 0:
@@ -116,20 +106,10 @@ class NGram(object):
                 break
             res += log(t, 2)
 
-
         return res
 
-    def prob(self, token, prev_tokens=None):
-        n = self.n
-        if not prev_tokens:
-            prev_tokens = []
-        assert len(prev_tokens) == n - 1
+class NGramGenerator(object):
 
-        tokens = prev_tokens + [token]
-        return float(self.counts[tuple(tokens)]) / self.counts[tuple(prev_tokens)]
-
-
-class NGramGenerator:
     def __init__(self, model):
         """
         model -- n-gram model (NGram object).
@@ -168,3 +148,108 @@ class NGramGenerator:
                 return k
             else:
                 acc += v
+
+
+class AddOneNGram(object):
+
+    def __init__(self, n, sents):
+        """
+        n -- order of the model.
+        sents -- list of sentences, each one being a list of tokens.
+        """
+        assert n > 0
+        self.n = n
+        self.counts = counts = defaultdict(int)
+        self.word_counts = defaultdict(int)
+        self.total_count = 0
+        self.probs = defaultdict(int)
+        self.sorted_probs = defaultdict(int)
+        self.number_token_options = defaultdict(int)
+
+        for i in range(len(sents)):
+            sents[i] = (['<s>'] * (n - 1)) + sents[i] + (['</s>'])
+
+        for sent in sents:
+            for i in range(len(sent) - n + 1):
+                self.word_counts[sent[i+n-1]] += 1
+                self.total_count += 1
+                ngram = tuple(sent[i: i + n])
+                counts[ngram] += 1
+                counts[ngram[:-1]] += 1
+                
+                if not ngram[:-1] in self.probs:
+                    self.probs[ngram[:-1]] = defaultdict(int)
+                (self.probs[ngram[:-1]])[ngram[len(ngram) - 1]] += 1
+                self.number_token_options[ngram[:-1]] += 1
+
+        for key, value in self.probs.items():
+            # Turn the number of occurrences into probabilities (that sum to 1,
+            # all together) inside each n-gram.
+            for k, v in value.items():
+                value[k] = v / float(self.number_token_options[key])
+
+        for key, value in self.probs.items():
+            # Once self.probs is fully processed, self.sorted_probs is easily
+            # obtainable.
+            self.sorted_probs[key] = sorted(list(value.items()), key=lambda x: (-x[1], x[0]))
+
+    def get_n(self):
+        return self.n
+
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        return self.counts[tokens]
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        if prev_tokens is None:
+            prev_tokens = []
+        
+        ngram = tuple((['<s>'] * (self.n - 1 - len(prev_tokens))) + prev_tokens + [token])
+        return (self.count(ngram) + 1) / float(self.count(tuple(prev_tokens)) + self.V())
+
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+
+
+        sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
+        
+        res = 1
+
+        for i in range(self.n - 1, len(sent) - self.n + 1):
+            res *= self.cond_prob(sent[i], sent[i - self.n  + 1: i])
+
+        return res
+
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+
+        sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
+        
+        res = 0
+
+        for i in range(self.n - 1, len(sent) - self.n + 1):
+            t = self.cond_prob(sent[i], sent[i - self.n  + 1: i])
+            if t == 0:
+                res = float('-inf')
+                break
+            res += log(t, 2)
+
+        return res
+
+    def V(self):
+        ''' Size of the vocabulary. '''
+        return (len(self.word_counts))
