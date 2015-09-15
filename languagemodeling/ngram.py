d5e2817b5121ee3e1504fa32f432204312450b9b
maico <maico@maico>
2015-09-15 16:43:12 -0300

    First non-finished implementation of class InterpolatedNGram (missing computation of self.gamma). Added a command line option for languagemodeling/scripts/train.py to train a linear interpolation model with this class.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -12,7 +12,7 @@ class NGram(object):
         """
         assert n > 0
         self.n = n
-        self.counts = counts = defaultdict(int)
+        self.counts = defaultdict(int)
         self.word_counts = defaultdict(int)
         self.total_count = 0
         self.probs = defaultdict(int)
@@ -27,10 +27,10 @@ class NGram(object):
                 self.word_counts[sent[i+n-1]] += 1
                 self.total_count += 1
                 ngram = tuple(sent[i: i + n])
-                counts[ngram] += 1
-                counts[ngram[:-1]] += 1
+                self.counts[ngram] += 1
+                self.counts[ngram[:-1]] += 1
                 
-                if not ngram[:-1] in self.probs:
+                if not ngram[:-1] in self.probs.keys():
                     self.probs[ngram[:-1]] = defaultdict(int)
                 (self.probs[ngram[:-1]])[ngram[len(ngram) - 1]] += 1
                 self.number_token_options[ngram[:-1]] += 1
@@ -47,14 +47,13 @@ class NGram(object):
             self.sorted_probs[key] = sorted(list(value.items()), key=lambda x: (-x[1], x[0]))
 
     def get_n(self):
-        return self.n
+        return (self.n)
 
     def count(self, tokens):
         """Count for an n-gram or (n-1)-gram.
  
         tokens -- the n-gram or (n-1)-gram tuple.
         """
-
         return (self.counts[tokens])
  
     def cond_prob(self, token, prev_tokens=None):
@@ -68,18 +67,20 @@ class NGram(object):
 
         ngram = tuple((['<s>'] * (self.n - 1 - len(prev_tokens))) + prev_tokens + [token])
         
-        if float(self.counts[tuple(prev_tokens)]) == 0:
-            return 0
+        if self.count(ngram) == 0:
+            # Result will be 0, even if self.count(tuple(prev_tokens)) == 0 
+            # (in which case this call to cond_prob should be undefined).
+            return (0)
         else:
-            return (self.counts[ngram] / float(self.counts[tuple(prev_tokens)]))
+            # If self.count(ngram) != 0, then 
+            #    self.count(tuple(prev_tokens)) != 0.
+            return ((self.count(ngram) / float(self.count(tuple(prev_tokens)))))
  
     def sent_prob(self, sent):
         """Probability of a sentence. Warning: subject to underflow problems.
  
         sent -- the sentence as a list of tokens.
         """
-
-
         sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
         
         res = 1
@@ -87,14 +88,13 @@ class NGram(object):
         for i in range(self.n - 1, len(sent) - self.n + 1):
             res *= self.cond_prob(sent[i], sent[i - self.n  + 1: i])
 
-        return res
+        return (res)
 
     def sent_log_prob(self, sent):
         """Log-probability of a sentence.
  
         sent -- the sentence as a list of tokens.
         """
-
         sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
         
         res = 0
@@ -106,7 +106,7 @@ class NGram(object):
                 break
             res += log(t, 2)
 
-        return res
+        return (res)
 
 class NGramGenerator(object):
 
@@ -133,7 +133,7 @@ class NGramGenerator(object):
             if current_token != '</s>':
                 sent = sent + [current_token,]
                 prev_token = (prev_token + (current_token,))[1 : self.n]
-        return sent
+        return (sent)
  
     def generate_token(self, prev_tokens=None):
         """Randomly generate a token, given prev_tokens.
@@ -145,7 +145,7 @@ class NGramGenerator(object):
         acc = 0.0
         for k, v in self.probs[prev_tokens].items():
             if acc + v >= x:
-                return k
+                return (k)
             else:
                 acc += v
 
@@ -159,12 +159,9 @@ class AddOneNGram(object):
         """
         assert n > 0
         self.n = n
-        self.counts = counts = defaultdict(int)
+        self.counts = defaultdict(int)
         self.word_counts = defaultdict(int)
         self.total_count = 0
-        self.probs = defaultdict(int)
-        self.sorted_probs = defaultdict(int)
-        self.number_token_options = defaultdict(int)
 
         for i in range(len(sents)):
             sents[i] = (['<s>'] * (n - 1)) + sents[i] + (['</s>'])
@@ -174,34 +171,18 @@ class AddOneNGram(object):
                 self.word_counts[sent[i+n-1]] += 1
                 self.total_count += 1
                 ngram = tuple(sent[i: i + n])
-                counts[ngram] += 1
-                counts[ngram[:-1]] += 1
+                self.counts[ngram] += 1
+                self.counts[ngram[:-1]] += 1
                 
-                if not ngram[:-1] in self.probs:
-                    self.probs[ngram[:-1]] = defaultdict(int)
-                (self.probs[ngram[:-1]])[ngram[len(ngram) - 1]] += 1
-                self.number_token_options[ngram[:-1]] += 1
-
-        for key, value in self.probs.items():
-            # Turn the number of occurrences into probabilities (that sum to 1,
-            # all together) inside each n-gram.
-            for k, v in value.items():
-                value[k] = v / float(self.number_token_options[key])
-
-        for key, value in self.probs.items():
-            # Once self.probs is fully processed, self.sorted_probs is easily
-            # obtainable.
-            self.sorted_probs[key] = sorted(list(value.items()), key=lambda x: (-x[1], x[0]))
-
     def get_n(self):
-        return self.n
+        return (self.n)
 
     def count(self, tokens):
         """Count for an n-gram or (n-1)-gram.
  
         tokens -- the n-gram or (n-1)-gram tuple.
         """
-        return self.counts[tokens]
+        return (self.counts[tokens])
 
     def cond_prob(self, token, prev_tokens=None):
         """Conditional probability of a token.
@@ -213,15 +194,13 @@ class AddOneNGram(object):
             prev_tokens = []
         
         ngram = tuple((['<s>'] * (self.n - 1 - len(prev_tokens))) + prev_tokens + [token])
-        return (self.count(ngram) + 1) / float(self.count(tuple(prev_tokens)) + self.V())
+        return ((self.count(ngram) + 1) / float(self.count(tuple(prev_tokens)) + self.V()))
 
     def sent_prob(self, sent):
         """Probability of a sentence. Warning: subject to underflow problems.
  
         sent -- the sentence as a list of tokens.
         """
-
-
         sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
         
         res = 1
@@ -229,14 +208,13 @@ class AddOneNGram(object):
         for i in range(self.n - 1, len(sent) - self.n + 1):
             res *= self.cond_prob(sent[i], sent[i - self.n  + 1: i])
 
-        return res
+        return (res)
 
     def sent_log_prob(self, sent):
         """Log-probability of a sentence.
  
         sent -- the sentence as a list of tokens.
         """
-
         sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
         
         res = 0
@@ -248,7 +226,149 @@ class AddOneNGram(object):
                 break
             res += log(t, 2)
 
-        return res
+        return (res)
+
+    def V(self):
+        """ Size of the vocabulary. """
+        return (len(self.word_counts))
+
+
+class InterpolatedNGram:
+ 
+    def __init__(self, n, sents, gamma=None, addone=True):
+        """
+        n -- order of the model.
+        sents -- list of sentences, each one being a list of tokens.
+        gamma -- interpolation hyper-parameter (if not given, estimate using
+            held-out data).
+        addone -- whether to use addone smoothing (default: True).
+        """
+        assert n > 0
+        self.n = n
+        # self.addone will be needed when computing self.cond_prob
+        self.addone = addone
+        if gamma is None:
+            # If gamma is not provided, use 90% of sents as training set, and
+            # last 10% as held-out data to estimate gamma.
+            training = sents[:int(len(sents) * 0.9)]
+            held_out = sents[int(len(sents) * 0.9):]
+            
+            # Compute gamma from held-out data.
+            # self.gamma = ???
+        else:
+            # If gamma is already provided, use sents as training set. No need
+            # for held-out data.
+            training = sents
+
+        self.counts = defaultdict(int)
+        self.word_counts = defaultdict(int)
+        self.total_count = 0
+        self.number_token_options = defaultdict(int)
+
+        for i in range(len(training)):
+            training[i] = (['<s>'] * (n - 1)) + training[i] + (['</s>'])
+
+        for sent in training:
+            for i in range(len(sent) - n + 1):
+                self.word_counts[sent[i+n-1]] += 1
+                self.total_count += 1
+                ngram = tuple(sent[i: i + n])
+                self.counts[ngram] += 1
+                self.counts[ngram[:-1]] += 1
+
+    def get_n(self):
+        return (self.n)
+
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+        return (self.counts[tokens])
+
+    def __max_likelyhood_prob(self, token, prev_tokens=None):
+        """ Conditional probability of a token, corresponding to the maximum 
+            likelyhood estimator used in the previous classes. 
+        """
+        if prev_tokens is None or len(prev_tokens) == 0:
+            prev_tokens = []
+            ngram = tuple([token])
+        else:
+            ngram = tuple((['<s>'] * (self.n - 1 - len(prev_tokens))) + prev_tokens + [token])
+
+        if self.addone and prev_tokens == []:
+            # Implementing add-one smoothing for the lower level (unigrams)
+            return ((self.count(ngram) + 1) / float(self.count(tuple()) + \
+                                                            self.__V()))
+        else:
+            if self.count(ngram) == 0:
+                # Result will be 0, even if self.count(tuple()) == 0
+                # (in which case this call to cond_prob will be undefined).
+                return (0)
+            else:
+                return (self.count(ngram) / float(self.count(tuple(prev_tokens))))
+
+    def __get_lambda(self, tokens, i, gamma, prev_lambdas=1):
+        """ Compute i-th lambda value from an (n-1)-gram (tokens), gamma and 
+            (1 - (1st lambda) - ... - ((i-2)-th lambda) - ((i-1)-th lambda)).
+        """
+        uple = tuple(tokens[:len(tokens) - i])
+        if len(uple) == 0:
+            return (prev_lambdas)
+        else:
+            return (prev_lambdas * self.count(uple) / float(self.count(uple) + gamma))
+
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        if prev_tokens is None:
+            prev_tokens = []
+        assert(len(prev_tokens) == self.n - 1)
+
+        prev_lambdas = 1
+        res = 0
+
+        for i in range(self.n):
+            current_lambda = self.__get_lambda(tokens=prev_tokens, i=i, gamma=self.gamma, prev_lambdas=prev_lambdas)
+            prev_lambdas = prev_lambdas - current_lambda
+            res += current_lambda * self.__max_likelyhood_prob(token=token, prev_tokens=prev_tokens[:len(prev_tokens) - i])
+
+        return (res)
+
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
+        res = 1
+
+        for i in range(self.n - 1, len(sent) - self.n + 1):
+            res *= self.cond_prob(sent[i], sent[i - self.n  + 1: i])
+
+        return (res)
+
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        sent = (['<s>'] * (self.n - 1)) + sent + ['</s>']
+        
+        res = 0
+
+        for i in range(self.n - 1, len(sent) - self.n + 1):
+            t = self.cond_prob(sent[i], sent[i - self.n  + 1: i])
+            if t == 0:
+                res = float('-inf')
+                break
+            res += log(t, 2)
+
+        return (res)
 
     def V(self):
         ''' Size of the vocabulary. '''
--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -9,67 +9,56 @@ Options:
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
-# from docopt import docopt
 from optparse import OptionParser
 import sys
 import pickle
 
 from nltk.corpus import brown
 
-from languagemodeling.ngram import NGram, AddOneNGram
+from languagemodeling.ngram import NGram, AddOneNGram, InterpolatedNGram
 
 
 if __name__ == '__main__':
-    # opts = docopt(__doc__)
-
-    # # load the data
-    # sents = brown.sents()
-
-    # # train the model
-    # n = int(opts['-n'])
-    # model = NGram(n, sents)
-
-    # # save it
-    # filename = opts['-o']
-    # f = open(filename, 'wb')
-    # pickle.dump(model, f)
-    # f.close()
-
     parser = OptionParser()
     parser.add_option("-o", dest="file", help="Output model file.", metavar="<file>")
     parser.add_option("-m", dest="model", help="Model to use [default: ngram]:\n \
                                                 \t\t\tngram: Unsmoothed n-grams.\
-                                                \t\t\taddone: N-grams with add-one smoothing.", metavar="<model>")
+                                                \t\t\taddone: N-grams with add-one smoothing.\
+                                                \t\t\tinterpolated: Smoothing by interpolation.", metavar="<model>")
     parser.add_option("-n", dest="n", help="Order of the model.", metavar="<n>")
     (options, args) = parser.parse_args()
     file_on = not options.file is None
     n_on = not options.n is None
 
     # Checking model option.
-    if options.model is None:
+    if (options.model is None):
         options.model = 'ngram'
-    if (options.model != 'ngram' and options.model != 'addone'):
+    if (options.model != 'ngram' and options.model != 'addone' and options.model != 'interpolated'):
         print("Invalid model.")
         parser.print_help()
         sys.exit(0)
 
     # Checking output file and order arguments.
-    if not file_on or not n_on:
+    if (not file_on or not n_on):
       print("Missing parameters:")
-    if not file_on:
+    if (not file_on):
         print("\t* Output model file")
-    if not n_on:
+    if (not n_on):
         print("\t* Order of the model")
-    if not file_on or not n_on:
+    if (not file_on or not n_on):
         sys.exit(0)
 
     sents = list(brown.sents())
     # print("type(sents): " + str(type(sents)))
     # assert(False)
-    if options.model == 'ngram':
+    if (options.model == 'ngram'):
         model = NGram(int(options.n), sents)
-    else:
+    elif (options.model == 'addone'):
         model = AddOneNGram(int(options.n), sents)
+    else:
+        # Creating a smoothing with interpolation model with default gamma 
+        # (estimated using held-out data) and unigram addone smoothing.
+        model = InterpolatedNGram(int(options.n), sents)
 
     try:
         f = open(options.file, "wb")
